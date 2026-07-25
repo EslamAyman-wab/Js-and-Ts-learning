6b04e359e0c14fdbe795c3875d1183e4c8f7606c
eslam <eslamayman2372002ii@gmail.com>
2026-07-25 22:32:23 +0300

    Remove unused files and add new JavaScript and markdown files for concatenation and template literals exercises

---
+++ Capter-02/ch_01/index.js
@@ -0,0 +1,24 @@
+// Add Variables Here
+
+let NumberOne = "10";
+let NumberTwo = 20;
+
+// Ouput
+console.log(NumberOne + NumberTwo); // Normal Concatenate => 1020
+console.log("Add Code Here"); // Normal Concatenate => String
+console.log("Add Code Here"); // Template Literals Way => 1020
+console.log("Add Code Here"); // Template Literals Way => String
+
+console.log("Add Code Here");
+/*
+  Normal Concatenate
+  20
+  10
+*/
+
+console.log("Add Code Here");
+/*
+  Template Literals Way
+  20
+  10
+*/
--- test/Main_01.js
+++
@@ -1,9 +0,0 @@
-console.log("Hello, World!");
-
-let a = 555;
-let b = 440;
-let c = a + b;
-console.log(c);
-
-console.table(["Apple", "Banana", "Cherry"]);
-console.log("This is a test %cmessage.", "color: blue; font-size: 16px;");
