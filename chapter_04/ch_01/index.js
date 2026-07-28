c6509aa503e2c654bb3275e05e11f1441089fbb9
EslamAyman <eslamayman2372002ii@gmail.com>
2026-07-28 21:21:36 +0300

    chapter 04 ch 01 done

---
+++ chapter_04/ch_01/index.js
@@ -0,0 +1,7 @@
+let userName = "Elzero";
+console.log(userName.substring(3,4)); // e
+console.log(userName.substr(3,1)); // e
+console.log(userName.slice(3,4)); // e
+console.log(); // e
+console.log(); // e
+console.log(userName.substring(3,4).repeat(3)); // eee
