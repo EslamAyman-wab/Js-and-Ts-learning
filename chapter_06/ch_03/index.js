8a3e8e85bb352495a88194d8e9bb28c9057c4555
EslamAyman <eslamayman2372002ii@gmail.com>
2026-08-01 23:12:49 +0300

    update

---
+++ chapter_06/ch_03/index.js
@@ -0,0 +1,8 @@
+let arrOne = ["C", "D", "X"];
+let arrTwo = ["A", "B", "Z"];
+let finalArr = [];
+
+// Write One Single Line Of Code
+finalArr.concat(arrOne, arrTwo);
+
+console.log(finalArr); // ["Z", "X", "D", "C", "B", "A"]
