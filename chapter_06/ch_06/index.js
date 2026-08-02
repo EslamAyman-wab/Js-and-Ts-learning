4e0a623d09ee1c67accb1922175c5b4caa1c2129
eslam <eslamayman2372002ii@gmail.com>
2026-08-02 14:31:01 +0300

    chapter 06 , ch 06 done

---
+++ chapter_06/ch_06/index.js
@@ -0,0 +1,14 @@
+let arr1 = ["A", "C", "X"];
+let arr2 = ["D", "E", "F", "Y"];
+let allArrs = [];
+
+// Your Code Here
+
+let name1 = arr1.pop(); // X
+let name2 = arr2.pop(); // Y
+let name3 = arr2.pop(); // F
+allArrs.push(name3);
+allArrs.push(name1);
+allArrs.push(name2);
+
+console.log(allArrs); // fxy
