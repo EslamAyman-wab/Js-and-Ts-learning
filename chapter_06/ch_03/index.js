03434458e8f66dc34b9fc719b6bbfc2c5962f281
eslam <eslamayman2372002ii@gmail.com>
2026-08-02 12:20:55 +0300

    chapter 06 , ch 03 done

--- chapter_06/ch_03/index.js
+++ chapter_06/ch_03/index.js
@@ -3,6 +3,7 @@ let arrTwo = ["A", "B", "Z"];
 let finalArr = [];
 
 // Write One Single Line Of Code
-finalArr.concat(arrOne, arrTwo);
+finalArr = arrOne.concat(arrTwo);
+finalArr.sort().reverse();
 
 console.log(finalArr); // ["Z", "X", "D", "C", "B", "A"]
