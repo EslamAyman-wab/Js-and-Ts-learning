6160074ed4e2b82169613e8fd616fd8062e51f6d
EslamAyman <eslamayman2372002ii@gmail.com>
2026-08-03 19:55:35 +0300

    chapter 07 ch 06 done

---
+++ chapter_07/ch_06/index.js
@@ -0,0 +1,28 @@
+let codeSmallA = "a".charCodeAt(0);
+let codeCapitalA = "A".charCodeAt(0);
+let difference = codeSmallA - codeCapitalA; // = 97 - 65 = 32
+
+let start = 0;
+let swappedName = "elZerO";
+
+let result = "";
+
+for (let i = start; i < swappedName.length; i++) {
+  let currentCharCode = swappedName.charCodeAt(i);
+  if (currentCharCode >= codeSmallA) {
+    // الحرف صغير → هحوله كبير
+    let newCharCode = currentCharCode - difference;
+    let newChar = String.fromCharCode(newCharCode);
+    result += newChar;
+  } else {
+    // الحرف كبير → هحوله صغير
+    let newCharCode = currentCharCode + difference;
+    let newChar = String.fromCharCode(newCharCode);
+    result += newChar;
+  }
+}
+
+console.log(result);
+
+// Output
+("ELzERo");
