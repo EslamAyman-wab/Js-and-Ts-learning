61996ceb6d484f9378f4a94c536214dfb58120cc
eslam <eslamayman2372002ii@gmail.com>
2026-08-03 15:14:12 +0300

    chapter 07 , ch 02 done

--- chapter_07/ch_02/index.js
+++ chapter_07/ch_02/index.js
@@ -1,1 +1,20 @@
-console.log("this is a test ");
+let start = 10;
+let end = 0;
+let stop = 3;
+
+for (let i = start; i > end; i--) {
+  i >= start ? console.log(`${i}`) : console.log(`${end + ""}${i}`);
+  if (i == stop) {
+    break;
+  }
+}
+
+// Output
+// 10
+// 09
+// 08
+// 07
+// 06
+// 05
+// 04
+// 03
