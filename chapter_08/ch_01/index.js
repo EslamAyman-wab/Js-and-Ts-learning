61e48bb5a03e2e3161e9be32324f79cef82911a3
eslam <eslamayman2372002ii@gmail.com>
2026-08-06 13:02:32 +0300

    chapter08 challenge 01 has been done

---
+++ chapter_08/ch_01/index.js
@@ -0,0 +1,15 @@
+function sayHello(theName, theGender) {
+    if (theGender === 'Male') {
+        console.log(`Hello Mr ${theName} `);
+    }  else if (theGender === 'Female') {
+        console.log(`Hello Miss ${theName}`);
+    }  else {
+        console.log(`Hello ${theName}`);
+    }
+    // Your Code Here
+}
+
+// Needed Output
+sayHello("Osama", "Male"); // "Hello Mr Osama"
+sayHello("Eman", "Female"); // "Hello Miss Eman"
+sayHello("Sameh"); // "Hello Sameh"
