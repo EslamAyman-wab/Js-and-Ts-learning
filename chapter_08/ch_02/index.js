388af8f8af36c450852fa80c51c1335633b67e50
eslam <eslamayman2372002ii@gmail.com>
2026-08-06 14:02:19 +0300

    chapter08 challenge 02 has been done

---
+++ chapter_08/ch_02/index.js
@@ -0,0 +1,28 @@
+function calculate(firstNum, secondNum, operation) {
+    if (secondNum === undefined) {
+        console.log(`Second Number Not Found`);
+    } else if(firstNum && secondNum && (operation=== undefined)) {
+        console.log(firstNum + secondNum);
+    } else {
+        switch (operation) {
+            case 'add':
+                console.log(firstNum +secondNum);
+                break;
+                case 'subtract':console.log(firstNum - secondNum);
+                break;
+                case 'multiply':console.log(firstNum * secondNum);
+                break;
+            default:
+                console.log(`the operation is ${operation} not found`);
+                break;
+        }
+    }
+    // Your Code Here
+}
+
+// Needed Output
+calculate(20); // Second Number Not Found
+calculate(20, 30); // 50
+calculate(20, 30, 'add'); // 50
+calculate(20, 30, 'subtract'); // -10
+calculate(20, 30, 'multiply'); // 600
