e9b2d99224104c8a1a863c4a32f54567a723d566
eslam <eslamayman2372002ii@gmail.com>
2026-08-06 16:10:43 +0300

    chapter08 challenge 06 has been done

---
+++ chapter_08/ch_06/index.js
@@ -0,0 +1,19 @@
+function multiply (...num){
+    let result = 1;
+    for (let i = 0; i < num.length; i++){
+        if(typeof num[i] === "string"){
+            continue;
+        } else if(!Number.isInteger(num[i])){
+           result*=parseInt(num[i]);
+        }
+        else {
+            result *= num[i];
+        }
+    }
+    return result;
+}
+
+
+console.log(multiply(10, 20)); // 200
+console.log(multiply("A", 10, 30)); // 300
+console.log(multiply(100.5, 10, "B")); // 1000
