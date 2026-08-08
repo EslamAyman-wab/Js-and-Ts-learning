8dca0bf6bb835bfe96ba7c6875b8bb160c450032
EslamAyman <eslamayman2372002ii@gmail.com>
2026-08-08 16:13:24 +0300

    chapter 09 challenge 02 has been done

---
+++ chapter_09/ch_02/index.js
@@ -0,0 +1,16 @@
+// function itsMe() {
+//     return `Iam A Normal Function`;
+// }
+//
+// console.log(itsMe()); // Iam A Normal Function
+let  itsMe= ()=>  `Iam A Normal Function`;
+console.log(itsMe())
+//
+// function urlCreate(protocol, web, tld) {
+//     return `${protocol}://www.${web}.${tld}`;
+// }
+//
+// console.log(urlCreate("https", "elzero", "org")); // https://www.elzero.org
+
+let urlCreate=(protocal , web , tld)=>`${protocal}://www.${web}.${tld}`;
+console.log(urlCreate("https","eslam","com"))
