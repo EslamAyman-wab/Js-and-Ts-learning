226b824564b7b5633919e6735d0381d1a72da922
EslamAyman <eslamayman2372002ii@gmail.com>
2026-08-09 22:14:48 +0300

    chapter 10 challenge 01 has been done

--- chapter_10/ch_01/index.js
+++ chapter_10/ch_01/index.js
@@ -2,7 +2,9 @@ let mix = [1, 2, 3, "E", 4, "l", "z", "e", "r", 5, "o"];
 
 let name = mix.map(function (ele){
     return isNaN(parseInt(ele)) ? ele : "";
-
-})
-console.log(name)
+});
+let result = name.reduce(function(acc, curr) {
+       return acc + curr;
+    },"");
+console.log(result)
 // Elzero
