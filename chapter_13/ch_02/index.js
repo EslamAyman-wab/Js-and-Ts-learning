5cb4082d3f7c6c1244948d498cbdab9e089aae62
eslam <eslamayman2372002ii@gmail.com>
2026-08-17 15:54:58 +0300

    update

---
+++ chapter_13/ch_02/index.js
@@ -0,0 +1,6 @@
+let myFriends = ["Osama", "Ahmed", "Sayed", "Sayed", "Mahmoud", "Osama"];
+
+let myNewSet = new Set(myFriends);
+console.log(myNewSet)
+// Needed Output
+(4) ['Ahmed', 'Mahmoud', 'Osama', 'Sayed']
