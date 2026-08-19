f7baa00ee6cacc43c5db16b19b56be43c16185aa
eslam <eslamayman2372002ii@gmail.com>
2026-08-19 14:45:53 +0300

    chapter 13 challenge 07 has been done

--- chapter_13/ch_05/index.js
+++ chapter_13/ch_05/index.js
@@ -1,6 +1,5 @@
 let theName = "Elzero";
 
 console.log([...theName])
-console.log(theName.map)
 // Needed Output
 // ['E', 'l', 'z', 'e', 'r', 'o']
---
+++ chapter_13/ch_07/index.js
@@ -0,0 +1,7 @@
+let numsOne = [1, 2, 3];
+let numsTwo = [4, 5, 6];
+
+let sum = [...numsOne,...numsTwo];
+console.log(sum)
+// Needed Output
+// [1, 2, 3, 4, 5, 6]
