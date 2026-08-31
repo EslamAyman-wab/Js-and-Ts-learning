5b117793dd2c5041a003f0eff091f225ccf70006
eslam <eslamayman2372002ii@gmail.com>
2026-08-31 15:52:51 +0300

    chapter 20 challenge 04 has been done update

--- chapter_20/ch_04/index.js
+++ chapter_20/ch_04/index.js
@@ -1,3 +1,11 @@
-let mydate = new Date("23/07/2002");
-let age = mydate.getFullYear()
-console.log(mydate)
+let Birthed1 = new Date("07/23/2002");
+console.log(Birthed1.toString())
+
+//
+let Birthed2= new Date(2002,6,23);
+console.log(Birthed2.toString())
+
+//
+let date = Date.parse("07/23/2002");
+let Birthed3 = new Date(date);
+console.log(Birthed3.toString())
