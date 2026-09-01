14d7f93570452998a928a48a2b5f070784716917
eslam <eslamayman2372002ii@gmail.com>
2026-09-01 14:10:46 +0300

    chapter 20 challenge 06 , 07 has been done

--- chapter_20/ch_06/index.js
+++ chapter_20/ch_06/index.js
@@ -1,1 +1,20 @@
-function*()
+function* gen(){
+    let value = 14;
+    let jump = 140;
+    while (true){
+        yield value;
+        value+=jump;
+        jump+=200;
+    }
+}
+let generator = gen();
+
+console.log(generator.next()); // {value: 14, done: false}
+console.log(generator.next()); // {value: 154, done: false}
+console.log(generator.next()); // {value: 494, done: false}
+console.log(generator.next()); // {value: 1034, done: false}
+console.log(generator.next()); // {value: 1774, done: false}
+console.log(generator.next()); // {value: 2714, done: false}
+console.log(generator.next()); // {value: 3854, done: false}
+console.log(generator.next()); // {value: 5194, done: false}
+console.log(generator.next()); // {value: 6734, done: false}
